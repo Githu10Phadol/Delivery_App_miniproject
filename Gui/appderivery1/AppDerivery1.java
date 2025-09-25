0f2424cac101c40e2b36e87aec1fff931a5158a6
Apichaikraiplong <apichai.kra@ku.th>
2025-09-25 13:19:40 +0700

    Add files via upload

---
+++ Gui/appderivery1/AppDerivery1.java
@@ -0,0 +1,19 @@
+
+package appderivery1;
+
+
+
+/**
+ *
+ * @author phadol
+ */
+public class AppDerivery1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    
+}
+}
